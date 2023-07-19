42de864f61235fe5bfb5ab6b594c164833bbc109
dancingkimDH <kimdonghyun0701@gmail.com>
2023-07-19 15:34:44 +0900

    update

--- src/main/java/com/example/baekgu_project/controller/CommonController.java
+++ src/main/java/com/example/baekgu_project/controller/CommonController.java
@@ -22,37 +22,37 @@ public class CommonController {
     @Autowired
     CommonService commonService;
 
-    @GetMapping({ "/my_pet_diary" })
+    @GetMapping("/my_pet_diary")
     public ModelAndView my_pet_diary(ModelAndView modelAndView) {
         modelAndView.setViewName("/WEB-INF/views/petdiary/my_pet_diary.jsp");
         return modelAndView;
     }
 
-    @GetMapping({ "/my_pet_manage" })
+    @GetMapping("/my_pet_manage")
     public ModelAndView my_pet_manage(ModelAndView modelAndView) {
         modelAndView.setViewName("/WEB-INF/views/petdiary/my_pet_manage.jsp");
         return modelAndView;
     }
 
-    @GetMapping({ "/new_pet" })
+    @GetMapping("/new_pet")
     public ModelAndView new_pet(ModelAndView modelAndView) {
         modelAndView.setViewName("/WEB-INF/views/petdiary/new_pet.jsp");
         return modelAndView;
     }
 
-    @GetMapping({ "/pet_introduce" })
+    @GetMapping("/pet_introduce")
     public ModelAndView pet_introduce(ModelAndView modelAndView) {
         modelAndView.setViewName("/WEB-INF/views/petdiary/pet_introduce.jsp");
         return modelAndView;
     }
 
-    @GetMapping({ "/statistics" })
+    @GetMapping("/statistics")
     public ModelAndView statistics(ModelAndView modelAndView) {
         modelAndView.setViewName("/WEB-INF/views/petdiary/statistics.jsp");
         return modelAndView;
     }
 
-    @GetMapping({ "/admin_main" })
+    @GetMapping("/admin_main")
     public ModelAndView admin_main(ModelAndView modelAndView) {
         modelAndView.setViewName("/WEB-INF/views/admin/admin_main.jsp");
         return modelAndView;
